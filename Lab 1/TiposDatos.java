a788b078ed07bfb1e400c458075ad338acda434e
Eduardo Martinez Jr <adonay.chacon12@gmail.com>
2024-01-31 15:22:17 -0600

    lab 1 primer ejercicio

---
+++ Lab 1/TiposDatos.java
@@ -0,0 +1,18 @@
+public class TiposDatos {
+    public static void main(String[] args) {
+        int var01 = 2000;
+        long var02 = 10000L;  // Se agrega una 'L' al final para indicar que es un valor long
+        float var03 = 6.5f;
+        double var04 = 10.7;
+        char var05 = '\u0022';  // Se asigna un valor constante Unicode (comillas dobles)
+        char var06 = 'k';  // Se asigna un valor constante caracter
+        boolean var07 = true;
+        System.out.println("var01 = " + var01);
+        System.out.println("var02 = " + var02);
+        System.out.println("var03 = " + var03);
+        System.out.println("var04 = " + var04);
+        System.out.println("var05 = " + var05);
+        System.out.println("var06 = " + var06);
+        System.out.println("var07 = " + var07);
+    }
+}
