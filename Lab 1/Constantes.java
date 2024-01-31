36ee7756092b7c464ae96e785d1e588b7499543f
Eduardo Martinez Jr <adonay.chacon12@gmail.com>
2024-01-31 17:04:30 -0600

    Ejercicio de string

---
+++ Lab 1/Constantes.java
@@ -0,0 +1,9 @@
+public class Constantes {
+    public static void main (String[] args){
+        final float pi =3.14159265F;
+        float radio = 3.5F;
+        //ouble area;
+        System.out.println("are circunferancia = "+(pi*radio*radio));
+
+    }
+}
